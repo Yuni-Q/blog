53b62ee495f8dd579e7d7040a226f08a498dfa03
Yuni-Q <carnotcycle2@naver.com>
2020-01-12 13:56:57 +0900

    fix: edit by yuniq's blog

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  title: `yuniq`,
+  description: `yuniq's graffiti`,
+  author: `yuniq`,
+  introduction: `yuniq's graffiti.`,
+  siteUrl: `https://yuni-q.github.io`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `Yuni-Q`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
   },
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
-    disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    disqusShortName: 'yuniq', // Your disqus-short-name. check disqus.com.
+    utterances: 'yuniq', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'yuniq',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
