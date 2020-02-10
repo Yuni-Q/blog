71759b5035d814891f61e1adb49c483890d55a2b
Yuni-Q <carnotcycle2@naver.com>
2020-02-10 12:29:42 +0900

    fix: fix title

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,29 +1,29 @@
 module.exports = {
-  title: `yuniq`,
-  description: `yuniq's graffiti`,
-  author: `yuniq`,
-  introduction: `yuniq's graffiti.`,
-  siteUrl: `https://yuni-q.github.io`, // Your blog site url
-  social: {
-    twitter: ``, // Your Twitter account
-    github: `Yuni-Q`, // Your GitHub account
-    medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
-  },
-  icon: `content/assets/felog.png`, // Add your favicon
-  keywords: [`blog`],
-  comment: {
-    disqusShortName: 'yuniq', // Your disqus-short-name. check disqus.com.
-    utterances: 'yuniq', // Your repository for archive comment
-  },
-  configs: {
-    countOfInitialPost: 10, // Config your initial count of post
-  },
-  sponsor: {
-    buyMeACoffeeId: 'yuniq',
-  },
-  share: {
-    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
-  },
-  ga: '', // Add your google analytics tranking ID
-}
+	title: `yuniq 블로그`,
+	description: `yuniq's graffiti`,
+	author: `yuniq`,
+	introduction: `yuniq's graffiti.`,
+	siteUrl: `https://yuni-q.github.io`, // Your blog site url
+	social: {
+		twitter: ``, // Your Twitter account
+		github: `Yuni-Q`, // Your GitHub account
+		medium: ``, // Your Medium account
+		facebook: ``, // Your Facebook account
+	},
+	icon: `content/assets/felog.png`, // Add your favicon
+	keywords: [`blog`],
+	comment: {
+		disqusShortName: 'yuniq', // Your disqus-short-name. check disqus.com.
+		utterances: 'yuniq', // Your repository for archive comment
+	},
+	configs: {
+		countOfInitialPost: 10, // Config your initial count of post
+	},
+	sponsor: {
+		buyMeACoffeeId: 'yuniq',
+	},
+	share: {
+		facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+	},
+	ga: '', // Add your google analytics tranking ID
+};
