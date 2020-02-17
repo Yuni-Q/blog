3065001a79baecec40fef7d03b74b995ed0ee4d6
Yuni-Q <carnotcycle2@naver.com>
2020-02-17 17:27:18 +0900

    fix: modify canvas size when resizing

--- src/layout/Christmas.jsx
+++ src/layout/Christmas.jsx
@@ -1,75 +1,101 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect } from 'react';
 
 export const ChristmasTheme = ({ children }) => {
-  const canvasRef = useRef(null)
+	const canvasRef = useRef(null);
 
-  useEffect(() => {
-    let snowflakes = []
-    let snowflake
-    const COUNT = 700
-    const canvasNode = canvasRef.current
-    const ctx = canvasNode.getContext('2d')
-    let i = 0
-    const width = document.documentElement.offsetWidth
-    const height = document.documentElement.offsetHeight
-    canvasNode.width = width
-    canvasNode.height = height
+	useEffect(() => {
+		const snow = () => {
+			let snowflakes = [];
+			let snowflake;
+			const COUNT = 700;
+			const canvasNode = canvasRef.current;
+			const ctx = canvasNode.getContext('2d');
+			let i = 0;
+			const width = document.documentElement.offsetWidth;
+			const height = document.documentElement.offsetHeight;
+			canvasNode.width = width;
+			canvasNode.height = height;
 
-    const reset = () => {
-      return {
-        x: Math.random() * width,
-        y: Math.random() * -height,
-        vy: 1 + Math.random() * 3,
-        vx: 0.5 - Math.random(),
-        r: 1 + Math.random() * 2,
-        opacity: 0.5 + Math.random() * 0.5,
-      }
-    }
+			const reset = () => {
+				return {
+					x: Math.random() * width,
+					y: Math.random() * -height,
+					vy: 1 + Math.random() * 3,
+					vx: 0.5 - Math.random(),
+					r: 1 + Math.random() * 2,
+					opacity: 0.5 + Math.random() * 0.5,
+				};
+			};
 
-    const init = () => {
-      ctx.fillStyle = '#FFF'
+			const init = () => {
+				ctx.fillStyle = '#FFF';
 
-      for (i = 0; i < COUNT; i++) {
-        snowflake = reset()
-        snowflakes.push(snowflake)
-      }
-      requestAnimationFrame(update)
-    }
+				for (i = 0; i < COUNT; i++) {
+					snowflake = reset();
+					snowflakes.push(snowflake);
+				}
+				requestAnimationFrame(update);
+			};
 
-    for (i = 0; i < COUNT; i++) {
-      snowflake = reset()
-      snowflakes.push(snowflake)
-    }
+			for (i = 0; i < COUNT; i++) {
+				snowflake = reset();
+				snowflakes.push(snowflake);
+			}
 
-    const update = () => {
-      ctx.clearRect(0, 0, width, height)
+			const update = () => {
+				ctx.clearRect(0, 0, width, height);
 
-      for (i = 0; i < COUNT; i++) {
-        snowflake = snowflakes[i]
-        snowflake.y += snowflake.vy
-        snowflake.x += snowflake.vx
+				for (i = 0; i < COUNT; i++) {
+					snowflake = snowflakes[i];
+					snowflake.y += snowflake.vy;
+					snowflake.x += snowflake.vx;
 
-        ctx.globalAlpha = snowflake.opacity
-        ctx.beginPath()
-        ctx.arc(snowflake.x, snowflake.y, snowflake.r, 0, Math.PI * 2, false)
-        ctx.closePath()
-        ctx.fill()
+					ctx.globalAlpha = snowflake.opacity;
+					ctx.beginPath();
+					ctx.arc(snowflake.x, snowflake.y, snowflake.r, 0, Math.PI * 2, false);
+					ctx.closePath();
+					ctx.fill();
 
-        if (snowflake.y > height) {
-          snowflake = reset()
-        }
-      }
+					if (snowflake.y > height) {
+						snowflake = reset();
+					}
+				}
 
-      requestAnimationFrame(update)
-    }
+				requestAnimationFrame(update);
+			};
+			init();
+		};
+		snow();
+		(function() {
+			const throttle = function(type, name, obj) {
+				obj = obj || window;
+				let running = false;
+				const func = function() {
+					if (running) {
+						return;
+					}
+					running = true;
+					requestAnimationFrame(function() {
+						obj.dispatchEvent(new CustomEvent(name));
+						running = false;
+					});
+				};
+				obj.addEventListener(type, func);
+			};
 
-    init()
-  }, [canvasRef])
+			/* init - you can init any event */
+			throttle('resize', 'optimizedResize');
+		})();
+		window.addEventListener('optimizedResize', snow);
+		return () => {
+			window.removeEventListener('optimizedResize', snow);
+		};
+	}, [canvasRef]);
 
-  return (
-    <>
-      <canvas className="snowflakes" ref={canvasRef} />
-      {children}
-    </>
-  )
-}
+	return (
+		<>
+			<canvas className="snowflakes" ref={canvasRef} />
+			{children}
+		</>
+	);
+};
