1df98a904ed331a9849328f96761f9ccc33d1671
Yuni-Q <carnotcycle2@naver.com>
2020-08-01 10:34:37 +0900

    utterances update

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -13,8 +13,8 @@ module.exports = {
 	icon: `content/assets/favicon.png`, // Add your favicon
 	keywords: [`blog`],
 	comment: {
-		disqusShortName: 'yuni-q', // Your disqus-short-name. check disqus.com.
-		utterances: 'blog', // Your repository for archive comment
+		disqusShortName: '', // Your disqus-short-name. check disqus.com.
+		utterances: 'Yuni-Q/blog', // Your repository for archive comment
 	},
 	configs: {
 		countOfInitialPost: 10, // Config your initial count of post
