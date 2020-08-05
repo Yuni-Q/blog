df83ec00b81e5b482c4007be7a95af44b2e0b647
Yuni-Q <carnotcycle2@naver.com>
2020-08-05 12:50:36 +0900

    tags page title

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,14 +4,11 @@ import { Link } from 'gatsby';
 import './index.scss';
 
 export const Header = ({ title, location, rootPath }) => {
-	const isRoot = location.pathname === rootPath;
 	return (
-		isRoot && (
-			<h1 className="home-header">
-				<Link to={`/`} className="link">
-					{title}
-				</Link>
-			</h1>
-		)
+		<h1 className="home-header">
+			<Link to={`/`} className="link">
+				{title}
+			</Link>
+		</h1>
 	);
 };
