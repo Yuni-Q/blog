1288c70fa4bb6002641e05893746d4a0e681e5ae
Yuni-Q <carnotcycle2@naver.com>
2020-08-18 02:30:28 +0900

    메모 페이지

---
+++ src/components/Memo.js
@@ -0,0 +1,153 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import './index.css';
+const Memo = ({
+	id,
+	top,
+	left,
+	width,
+	height,
+	text,
+	onClickDelete,
+	onClickMove,
+	onClickResize,
+	onChangeText,
+}) => {
+	const [value, setValue] = useState(text);
+	const x = useRef(null);
+	const y = useRef(null);
+	const headerRef = useRef(null);
+	const blindRef = useRef(null);
+	const isHeaderRef = useRef(null);
+	const isBlindRef = useRef(null);
+	const memoRef = useRef(null);
+	const textareaRef = useRef(null);
+
+	const downMouseHeader = useCallback(
+		event => {
+			if (event.target.dataset.delete) {
+				onClickDelete(event);
+				return;
+			}
+			if (event.target.dataset.header) {
+				x.current = event.clientX - left;
+				y.current = event.clientY - top;
+				isHeaderRef.current = true;
+				return;
+			}
+		},
+		[onClickDelete, left, top]
+	);
+
+	const onClickBlind = event => {
+		if (event.target.dataset.blind) {
+			isBlindRef.current = true;
+		}
+	};
+
+	const moveMouse = useCallback(
+		event => {
+			const momeStyle = memoRef.current.style;
+			if (isHeaderRef.current) {
+				const momeStyle = memoRef.current.style;
+				momeStyle.top = `${event.clientY - y.current}px`;
+				momeStyle.left = `${event.clientX - x.current}px`;
+				momeStyle.zIndex = '1';
+			}
+			if (isBlindRef.current) {
+				const textareaStyle = textareaRef.current.style;
+				textareaStyle.height = `${event.clientY - top - 30}px`;
+				textareaStyle.width = `${event.clientX - left - 15}px`;
+				momeStyle.zIndex = '1';
+			}
+		},
+		[top, left]
+	);
+
+	const upMouseHeader = useCallback(() => {
+		onClickMove(id, memoRef.current.style.top, memoRef.current.style.left);
+		isHeaderRef.current = false;
+	}, [id, onClickMove]);
+
+	const upMouseBlind = useCallback(() => {
+		isBlindRef.current = false;
+		onClickResize(
+			id,
+			textareaRef.current.style.width,
+			textareaRef.current.style.height
+		);
+	}, [id, onClickResize]);
+
+	useEffect(() => {
+		const header = headerRef.current;
+		const blind = blindRef.current;
+		header.addEventListener('mousedown', downMouseHeader, true);
+		window.addEventListener('mousemove', moveMouse, true);
+		header.addEventListener('mouseup', upMouseHeader, true);
+		blind.addEventListener('mousedown', onClickBlind, true);
+		blind.addEventListener('mouseup', upMouseBlind, true);
+		window.addEventListener('beforeunload', () =>
+			document.activeElement.blur()
+		);
+
+		return () => {
+			header.removeEventListener('mousedown', downMouseHeader, true);
+			window.removeEventListener('mousemove', moveMouse, true);
+			header.removeEventListener('mouseup', upMouseHeader, true);
+			blind.removeEventListener('mousedown', onClickBlind, true);
+			blind.removeEventListener('mouseup', upMouseBlind, true);
+		};
+	}, [
+		downMouseHeader,
+		id,
+		moveMouse,
+		onChangeText,
+		upMouseBlind,
+		upMouseHeader,
+		value,
+	]);
+	return (
+		<div
+			className="memo"
+			ref={memoRef}
+			style={{ top: `${top}px`, left: `${left}px` }}
+		>
+			<div
+				className="header"
+				data-header={true}
+				data-id={id}
+				ref={headerRef}
+				onClick={downMouseHeader}
+			>
+				<h1 className="blind">메모장</h1>
+				<button
+					className="btn_close"
+					data-delete={true}
+					data-id={id}
+					onClick={onClickDelete}
+				>
+					<span className="blind">닫기</span>
+				</button>
+			</div>
+			<div className="content">
+				<textarea
+					className="textarea"
+					ref={textareaRef}
+					style={{ width: `${width}px`, height: `${height}px` }}
+					placeholder="메모 하십시오!"
+					value={value}
+					onChange={e => {
+						setValue(e.target.value);
+					}}
+					onBlur={() => {
+						onChangeText(id, value);
+					}}
+				/>
+				<button className="btn_size" data-blind={true} ref={blindRef}>
+					<span className="blind">메모장 크기 조절</span>
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default Memo;
---
+++ src/pages/memo.tsx
@@ -0,0 +1,152 @@
+import React, { useCallback, useEffect, useRef, useState } from "react";
+
+import Memo from "../components/Memo";
+import axios from 'axios';
+const { v4: uuid } = require('uuid');
+
+const App = () => {
+  
+  const [memos, setMemos] = useState([]);
+
+  const ref = useRef(null);
+
+  const onClickAdd = useCallback(
+    event => {
+      event.preventDefault();
+      if (event.target.dataset.wrap) {
+        setMemos([
+          ...memos,
+          {
+            id: uuid(),
+            top: (window.event as any).clientY,
+            left: (window.event as any).clientX,
+            width: 200,
+            height: 100,
+            text: '',
+          }
+        ]);
+      }
+    },
+    [memos]
+  );
+
+  const onClickDelete = event => {
+    const newMomos = memos.filter(memo => {
+      return memo.id !== event.target.dataset.id;
+    });
+    setMemos(newMomos);
+  };
+
+  const onClickMove = (id, top, left) => {
+    const memo = memos.find(memo => {
+      return memo.id === id;
+    });
+    const newMomos = memos.filter(memo => {
+      return memo.id !== id;
+    });
+    setMemos([
+      ...newMomos,
+      {
+        id: uuid(),
+        top: parseInt(top, 10),
+        left: parseInt(left, 10),
+        width: memo.width,
+        height: memo.height,
+        text: '',
+      }
+    ]);
+  };
+
+  const onClickResize = (id, width, height) => {
+    const memo = memos.find(memo => {
+      return memo.id === id;
+    });
+    const newMomos = memos.filter(memo => {
+      return memo.id !== id;
+    });
+    setMemos([
+      ...newMomos,
+      {
+        id: uuid(),
+        top: memo.top,
+        left: memo.left,
+        width: parseInt(width, 10),
+        height: parseInt(height, 10),
+        text: '',
+      }
+    ]);
+  };
+  const onChangeText = (id, text) => {
+    const memo = memos.find(memo => {
+      return memo.id === id;
+    });
+    const newMomos = memos.filter(memo => {
+      return memo.id !== id;
+    });
+    setMemos([
+      ...newMomos,
+      {
+        id: uuid(),
+        top: memo.top,
+        left: memo.left,
+        width: memo.width,
+        height: memo.height,
+        text: text,
+      }
+    ]);
+  }
+
+  const getItem = async () => {
+    const result = await axios.get('https://script.google.com/macros/s/AKfycbwn9aYX70mvprKz1IbJezxqzXCDP2-24tjJ9qdjIqlcgOkLsshg/exec');
+    setMemos(JSON.parse(result.data.items[0].name) || [])
+  }
+  
+  const setItem = async (memos) => {
+    await axios.post(`https://script.google.com/macros/s/AKfycbwn9aYX70mvprKz1IbJezxqzXCDP2-24tjJ9qdjIqlcgOkLsshg/exec?data=${memos}`)
+  }
+
+  useEffect(() => {
+    setItem(JSON.stringify(memos))
+  }, [memos, onClickAdd]);
+
+  useEffect(() => {
+    const wrapRef = ref.current;
+    wrapRef.addEventListener("contextmenu", onClickAdd, true);
+    return () => wrapRef.removeEventListener("contextmenu", onClickAdd, true);
+  }, [onClickAdd]);
+
+  useEffect(() => {
+    getItem();
+  },[])
+
+  return (
+    <div
+      className="wrap"
+      data-wrap={true}
+      ref={ref}
+      style={{ width: "100vw", height: "100vh" }}
+    >
+      {memos &&
+        memos.map(memo => {
+          const { id, top, left, width, height,text } = memo;
+          return (
+            <Memo
+              key={id}
+              id={id}
+              top={top}
+              left={left}
+              width={width}
+              height={height}
+              text={text}
+              onClickDelete={onClickDelete}
+              onClickMove={onClickMove}
+              onClickResize={onClickResize}
+              onChangeText={onChangeText}
+            />
+          );
+        })}
+    </div>
+  );
+};
+
+export default App;
