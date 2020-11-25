4e366da7a1f39362f101863d647f0e22c7c88030
Yuni-Q <lyh6425@gmail.com>
2020-11-25 11:45:43 +0900

    list-style-type 추가

--- src/components/post-container/index.tsx
+++ src/components/post-container/index.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import styled from 'styled-components';
 
-const StyledDiv = styled.div`
-  ul {
-    list-style-type: none;
-  }
-`;
+const StyledDiv = styled.div``;
 
 export const PostContainer = ({ html }) => (
   <StyledDiv className="post-content" dangerouslySetInnerHTML={{ __html: html }} />
