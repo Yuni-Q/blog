f22e907e4de0ff0dba1c71b307fa8d824d58d848
Yuni-Q <lyh6425@gmail.com>
2021-01-31 23:59:35 +0900

    hr 생략

--- src/components/post-container/index.tsx
+++ src/components/post-container/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import styled from 'styled-components';
 
-const StyledDiv = styled.div``;
+const StyledDiv = styled.div`
+  hr {
+    display: none;
+  }
+`;
 
 export const PostContainer = ({ html }) => (
   <StyledDiv className="post-content" dangerouslySetInnerHTML={{ __html: html }} />
