aeca00df8d8f5953ec82bf8dc70d12868f907fa0
Yuni-Q <lyh6425@gmail.com>
2021-02-08 00:53:10 +0900

    lint 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,21 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    node: true,
   },
-  plugins: ['react'],
-  globals: {
-    graphql: false,
-  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: 'module',
     ecmaFeatures: {
-      experimentalObjectRestSpread: true,
       jsx: true,
     },
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
-}
+  plugins: ['react', '@typescript-eslint'],
+  // 0 = off, 1 = warning, 2 = error (you passed "3")
+  rules: {
+    'react/jsx-key': 2,
+    semi: [2, 'never'],
+  },
+};
