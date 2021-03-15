13bd7d653444b4f43cd01879112adc511a15f547
Yuni-Q <lyh6425@gmail.com>
2021-03-15 23:05:38 +0900

    adsense

--- src/html.tsx
+++ src/html.tsx
@@ -2,38 +2,43 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 interface Props {
-	htmlAttributes: object;
-	headComponents: Array<any>;
-	bodyAttributes: object;
-	preBodyComponents: Array<any>;
-	body: string;
-	postBodyComponents: Array<any>;
+  htmlAttributes: object;
+  headComponents: Array<any>;
+  bodyAttributes: object;
+  preBodyComponents: Array<any>;
+  body: string;
+  postBodyComponents: Array<any>;
 }
 
 export default class HTML extends React.Component<Props> {
-	render() {
-		return (
-			<html {...this.props.htmlAttributes}>
-				<head>
-					<meta charSet="utf-8" />
-					<meta httpEquiv="x-ua-compatible" content="ie=edge" />
-					<meta
-						name="viewport"
-						content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1, maximum-scale=2"
-					/>
-					{this.props.headComponents}
-				</head>
-				<body {...this.props.bodyAttributes}>
-					{this.props.preBodyComponents}
-					<noscript>You need to enable JavaScript to run this app.</noscript>
-					<div
-						key={`body`}
-						id="___gatsby"
-						dangerouslySetInnerHTML={{ __html: this.props.body }}
-					/>
-					{this.props.postBodyComponents}
-				</body>
-			</html>
-		);
-	}
+  render() {
+    return (
+      <html {...this.props.htmlAttributes}>
+        <head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1, maximum-scale=2"
+          />
+          {this.props.headComponents}
+          <script
+            data-ad-client="ca-pub-2667251850399676"
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          ></script>
+        </head>
+        <body {...this.props.bodyAttributes}>
+          {this.props.preBodyComponents}
+          <noscript>You need to enable JavaScript to run this app.</noscript>
+          <div
+            key={`body`}
+            id="___gatsby"
+            dangerouslySetInnerHTML={{ __html: this.props.body }}
+          />
+          {this.props.postBodyComponents}
+        </body>
+      </html>
+    );
+  }
 }
