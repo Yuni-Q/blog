4d6ab093e9b707969053e947edf8f700a6b5b1a1
Yuni-Q <lyh6425@gmail.com>
2021-06-07 01:55:29 +0900

    addthis 추가

--- src/components/social-share/index.tsx
+++ src/components/social-share/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FacebookIcon } from './facebook-icon';
 import { TwitterIcon } from './twitter-icon';
 import { shareToTwitter, shareToFacebook } from '../../utils/share';
@@ -6,6 +6,16 @@ import { shareToTwitter, shareToFacebook } from '../../utils/share';
 import './index.scss';
 
 export const SocialShare = ({ title, author }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      const addthisScript = document.createElement('script');
+      addthisScript.setAttribute(
+        'src',
+        'http:////s7.addthis.com/js/300/addthis_widget.js#pubid=ra-60bcf43de82361a8',
+      );
+      if (document.body) document.body.appendChild(addthisScript);
+    });
+  }, []);
   const text = `Recommend on "${title}" written by @${author}`;
 
   const onClickTwitterIcon = (e) => {
@@ -20,9 +30,15 @@ export const SocialShare = ({ title, author }) => {
   };
 
   return (
-    <div className="social-share">
-      <FacebookIcon onClick={onClickFacebookIcon} />
-      <TwitterIcon onClick={onClickTwitterIcon} />
+    <div className="social-share" style={{ marginTop: 32 }}>
+      <div className="addthis_inline_share_toolbox_7fpx"></div>
+      {/* <!-- Go to www.addthis.com/dashboard to customize your tools --> */}
+      <script
+        type="text/javascript"
+        src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-60bcf43de82361a8"
+      ></script>
+      {/* <FacebookIcon onClick={onClickFacebookIcon} />
+      <TwitterIcon onClick={onClickTwitterIcon} /> */}
     </div>
   );
 };
