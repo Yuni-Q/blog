22403eff9e38d43ef714dd8ecc7dee4e51860d04
Yuni-Q <lyh6425@gmail.com>
2022-03-07 01:00:33 +0900

    TILTIL

--- src/components/about/sideProject/TILTIL.tsx
+++ src/components/about/sideProject/TILTIL.tsx
@@ -14,7 +14,29 @@ export const TILTIL: VFC = () => {
         </a>
       </h3>
       <ul>
-        <li>오늘보다 더 나은 내일의 나를 위해 암묵지 쌓기</li>
+        <li>
+          <a href="https://bing-bong.today/" target="_blank" rel="noreferrer">
+            웹사이트
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.wanted.co.kr/community/post/3251?fbclid=IwAR17fEP_2Pyws6zDu6uo9KYq92YA3_vtk9GzVxKMYOQWkzY8AaCBOhYz1dE"
+            target="_blank"
+            rel="noreferrer"
+          >
+            서비스 소개 및 회고
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.wanted.co.kr/community/post/2612?fbclid=IwAR3Fh7QLJ8WLJebN9kXzEQuWnrK_oIaGP_nDA2FuKCYIXNJYW2wJOyDmAWs"
+            target="_blank"
+            rel="noreferrer"
+          >
+            해커톤 회고록
+          </a>
+        </li>
         <li>기간 : 2022년 2월</li>
         <li>역할 : 프론트 개발</li>
         <li>
