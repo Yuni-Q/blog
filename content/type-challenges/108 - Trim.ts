c22b4d7be791d509ec43ea2d8b587e6e172fed8d
Yuni-Q <lyh6425@gmail.com>
2022-10-03 22:49:53 +0900

    108 - Trim

---
+++ content/type-challenges/108 - Trim.ts
@@ -0,0 +1,7 @@
+// type Trim<S extends string> = S extends `${' '|'\n'|'\t'}${infer T}${' '|'\n'|'\t'}`? Trim<T> : S;
+// 한번에 제거할 수 없나...?
+type TrimRight<S extends string> = S extends `${infer T}${' ' | '\n' | '\t'}`
+  ? TrimRight<T>
+  : S;
+
+type Trim<S extends string> = TrimLeft<TrimRight<S>>;
