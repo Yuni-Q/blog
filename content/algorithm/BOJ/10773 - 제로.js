418b288cc5463b2e208961853f814f36989f0bfb
Yuni-Q <carnotcycle2@naver.com>
2022-12-12 14:08:09 +0900

    알고리즘 10773 - 제로

--- content/algorithm/BOJ/10773 - 제로.js
+++ content/algorithm/BOJ/10773 - 제로.js
@@ -1,7 +1,8 @@
 const input = ['10', '1', '3', '5', '4', '0', '0', '7', '0', '0', '6'];
+// const input = ['4', '3', '0', '4', '0'];
 
 const result = [];
-for (let i = 1; input.length < i; i++) {
+for (let i = 1; i < input.length; i++) {
   const num = Number(input[i]);
   if (num) {
     result.push(num);
